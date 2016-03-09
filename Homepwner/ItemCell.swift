6119c4dbebe7610f52b6ac716349854dce2a8169
Jackie Zhang <viperking@163.com>
2016-03-10 00:33:45 +0800

    changed row height to automatic

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Jackie Zhang on 16/3/8.
+//  Copyright © 2016年 Jackie Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell : UITableViewCell {
+    @IBOutlet var nameLabel : UILabel!
+    @IBOutlet var serialNumberLabel : UILabel!
+    @IBOutlet var valueLabel : UILabel!
+    
+}
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -19,6 +19,8 @@ class ItemsViewController : UITableViewController {
         let insets = UIEdgeInsets(top: statusBarHeight, left : 0, bottom: 0, right: 0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 65
         
     }
     
@@ -30,10 +32,13 @@ class ItemsViewController : UITableViewController {
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("ItemCell", forIndexPath: indexPath) as! ItemCell
         let item = itemStore.allItems[indexPath.row]
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+       
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+        cell.valueLabel.text =  "\(item.valueInDollars)"
+        
         return cell
        
     }
