cf7a4b2445152ba9c690619c3699e5f0b5bf99dc
张泽 <gjjisgod@gmail.com>
2016-03-18 17:52:45 +0800

    added  debug  log

--- Homepwner/AppDelegate.swift
+++ Homepwner/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
+        //display bundle path
+        print (NSBundle.mainBundle().bundlePath)
+        
         //Create Image Store
         let imageStore =  ImageStore()
         
