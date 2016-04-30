0373af46b902e3a6713da05d17669984a76ce0bd
Jackie Zhang <viperking@163.com>
2016-04-30 15:30:00 +0800

    moved EasyToDo folder

---
+++ EasyToDoProject/EasyToDo/ItemCell.swift
@@ -0,0 +1,48 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Jackie Zhang on 16/3/8.
+//  Copyright © 2016年 Jackie Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell : UITableViewCell {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet var notifyDateLabel: UILabel!
+
+    @IBOutlet var doneButton: UIButton!
+    
+    var expired : Bool = false
+    
+    func updateLabels(finished: Bool, expired: Bool){
+
+        //finished item will not be "Done"able
+        if (finished) {
+            doneButton.alpha = 0.0
+            doneButton.enabled = false
+            textField.textColor = UIColor.grayColor()
+        } else {
+            doneButton.alpha = 1.0
+            doneButton.enabled = true
+            textField.textColor = UIColor.blackColor()
+        }
+        
+        if expired { //expired notify item
+            textField.textColor = UIColor.redColor()
+            self.expired = true
+        }
+
+
+
+        //update font setting
+        let bodyFont = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
+        textField.font = bodyFont
+        
+        notifyDateLabel.textColor = UIColor.grayColor()
+        
+    }
+    
+}
---
+++ EasyToDoProject/EasyToDo/PictureViewController.swift
@@ -0,0 +1,147 @@
+//
+//  PictureViewController.swift
+//  Homepwner
+//
+//  Created by Jackie Zhang on 16/4/20.
+//  Copyright © 2016年 Jackie Zhang. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController : UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UIScrollViewDelegate
+ {
+    
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet var scrollView: ImageScrollView!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var doubleTapGestureRecognizer: UITapGestureRecognizer!
+    
+    @IBOutlet var twoFingerTapGestureRecognizer: UITapGestureRecognizer!
+    
+    var item:Item! {
+        didSet {
+            navigationItem.title = item.name
+        }
+    }
+    
+    var imageStore: ImageStore!
+    
+    override func viewDidLayoutSubviews() {
+        //need to put code to use scrollview frame here due to it will be 600x600 in viewwillappear
+        if imageView.image != nil {
+            scrollView.contentSize = imageView.image!.size
+            let scrollViewFrame = scrollView.frame
+            let scaleWidth = scrollViewFrame.size.width / scrollView.contentSize.width
+            let scaleHeight = scrollViewFrame.size.height / scrollView.contentSize.height;
+            let minScale = min(scaleWidth, scaleHeight);
+            scrollView.minimumZoomScale = minScale;
+            
+            scrollView.maximumZoomScale = 1.0;
+            scrollView.setZoomScale(minScale, animated: true)
+
+        }
+       
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        //get the image key
+         let key = item.itemKey
+        //if there is associated image , display it on image view
+         let imageToDisplay = imageStore.imageForKey(key)
+        imageView.image = imageToDisplay
+        scrollView.imageView = imageView
+
+    }
+    
+    //MARK: - controller
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        //get image from info directory
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        //put image into cache
+        imageStore.setImage(image, forKey: item.itemKey)
+        
+        //put the image into imageview
+        imageView.image = image
+        scrollView.imageView.image = image
+        
+        //take imagePicker off screen
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    func navigationController(navigationController: UINavigationController, willShowViewController viewController: UIViewController, animated: Bool) {
+        if(imagePicker.sourceType == UIImagePickerControllerSourceType.Camera){
+            let button = UIBarButtonItem(title: "Choose picture", style: UIBarButtonItemStyle.Plain, target: self, action: #selector(choosePicture))
+            viewController.navigationItem.rightBarButtonItem = button
+            viewController.navigationController?.navigationBarHidden = false
+            viewController.navigationController?.navigationBar.translucent = true
+        }
+    }
+    
+    //MARK: - customized action
+    func showCamera(){
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+    }
+    
+    func choosePicture(){
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+    }
+    @IBAction func takePicture(sender: UIBarButtonItem) {
+        
+        
+        //see if camera supported, if not , pick from library
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            imagePicker.sourceType  = .Camera
+        }
+        else {
+            imagePicker.sourceType = .PhotoLibrary
+        }
+        
+        imagePicker.delegate = self
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+
+    //MARK: - zooming logic
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidEndZooming(scrollView: UIScrollView, withView view: UIView?, atScale scale: CGFloat) {
+ 
+ }
+    
+    //MARK: - gesture logic
+    @IBAction func twoFingerTapped(sender: UITapGestureRecognizer) {
+         var newZoomScale = scrollView.zoomScale / 1.5
+        newZoomScale = max(newZoomScale, scrollView.minimumZoomScale);
+        //imageView.alpha = 0
+        scrollView.setZoomScale(newZoomScale, animated: true)
+        
+    }
+    
+    @IBAction func doubleTapped(sender: UITapGestureRecognizer) {
+        print ("double tapped");
+       
+        let pointInView = doubleTapGestureRecognizer.locationInView(imageView)
+        
+        var newZoomScale = self.scrollView.zoomScale * 1.5;
+        newZoomScale = min(newZoomScale, scrollView.maximumZoomScale);
+        
+        let scrollViewSize = scrollView.bounds.size;
+        
+        let w = scrollViewSize.width / newZoomScale;
+        let h = scrollViewSize.height / newZoomScale;
+        let x = pointInView.x - (w / 2.0);
+        let y = pointInView.y - (h / 2.0);
+        
+        let rectToZoomTo = CGRectMake(x, y, w, h);
+        
+        //imageView.alpha = 0
+        scrollView.zoomToRect(rectToZoomTo, animated: true)
+        
+    }
+    
+}
