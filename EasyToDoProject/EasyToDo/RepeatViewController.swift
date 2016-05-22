36f4d0bd6a50b0fbaa73deceedb5893fbda302bd
Jackie Zhang <viperking@163.com>
2016-05-22 16:37:21 +0800

    added style in repeat view controller

--- EasyToDoProject/EasyToDo/RepeatViewController.swift
+++ EasyToDoProject/EasyToDo/RepeatViewController.swift
@@ -19,6 +19,20 @@ class RepeatViewController : UITableViewController, UITextFieldDelegate,  UIText
         return 1
     }
     
+    override func viewWillAppear(animated: Bool) {
+        tableView.backgroundColor = UIColor(red: 206.0/255.0, green: 203.0/255.0, blue: 188.0/255.0, alpha: 1.0)
+        tableView.sectionIndexColor = UIColor.whiteColor()
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        let c = cell as! BaseCell
+        c.InitCellViews()
+        //  c.openAnimation(0.0,completion: nil)
+        
+        
+    }
+
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("repeatCell", forIndexPath: indexPath) as! DetailDateRepeatCell
         cell.dateRepeatLabel.text = AppDelegate.RepeatTime[indexPath.row]
