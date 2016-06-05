6f8310c3128b31c5345c41b9483c10c2825461d4
Jackie Zhang <viperking@163.com>
2016-06-05 15:57:29 +0800

    finished basic recording/playing/ metering logic

--- EasyToDoProject/EasyToDo/AudioViewController.swift
+++ EasyToDoProject/EasyToDo/AudioViewController.swift
@@ -14,16 +14,32 @@ class AudioViewController : UIViewController, UINavigationControllerDelegate, AV
     @IBOutlet var startRecordBtn: UIButton!
     @IBOutlet var stopRecordBtn: UIButton!
     @IBOutlet var startPlayBtn: UIButton!
-    @IBOutlet var stopPlayBtn: UIButton!
     
     var audioRecorder: AVAudioRecorder!
     var audioPlayer: AVAudioPlayer!
+    
+    @IBOutlet var meteringLabel: UILabel!
     
     var audioSession : AVAudioSession!
     
     var audioStore : AudioStore!
     
     var item: Item!
+    
+    var isPlaying: Bool = false
+    var isRecording: Bool = false
+    
+    //audio button
+    var audioPlayLayer = CAShapeLayer()
+    var audioPlayPath = UIBezierPath()
+    var audioPausePath = UIBezierPath()
+    
+    var audioRecordLayer = CAShapeLayer()
+    var audioRecordPath = UIBezierPath()
+    var audioStopRecordPath = UIBezierPath()
+    
+    //nstimer used to show audio recording/playing metering
+    var timer : NSTimer?
     
     let recordSettings = [
         AVSampleRateKey : NSNumber(float: Float(44100.0)),
@@ -32,58 +48,228 @@ class AudioViewController : UIViewController, UINavigationControllerDelegate, AV
         AVEncoderAudioQualityKey : NSNumber(int: Int32(AVAudioQuality.Medium.rawValue))
     ]
     
-    override func viewDidLoad() {
-        
-        audioSession = AVAudioSession.sharedInstance()
-        do {
-                try audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
-            
-        } catch {
-            print ("audio recorder prepare failed: \(error)")
-        }
-        
-    }
     
-    func directoryURL() -> NSURL? {
+    var directoryURL : NSURL? {
         if let it = item {
             return audioStore.audioURLForKey(it.itemKey)
         }
         return nil
         
     }
+    
+    //MARK: - Initializing
+    
+    override func viewDidLoad() {
+        
+        audioSession = AVAudioSession.sharedInstance()
+        do {
+            
+            try audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            
+        } catch {
+            print ("audio recorder prepare failed: \(error)")
+        }
+        
+        
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        view.backgroundColor = AppDelegate.backColor
+        
+        initAudioPlayPath()
+        initAudioPausePath()
+        initAudioRecordPath()
+        initAudioStopRecordPath()
+        
+        initPlayButtonView(true)
+        initRecordButtonView(true)
+    }
+    
+    //MARK: - Audio metering
+    func startAudioMetering() {
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: #selector(updateMetering), userInfo: nil, repeats: true)
+    }
+    
+    func stopAudioMetering() {
+        timer?.invalidate()
+        meteringLabel.text = ""
+    }
+    
+    func updateMetering() {
+        
+        var dbLevel : Float = 0.0
+        
+        if isRecording {
+            self.audioRecorder.updateMeters()
+            dbLevel = self.audioRecorder.averagePowerForChannel(0)
+        } else if isPlaying {
+            self.audioPlayer.updateMeters()
+            dbLevel = self.audioPlayer.averagePowerForChannel(0)
+        }
+        
+        performSelectorOnMainThread(#selector(updateMeteringUI), withObject: dbLevel, waitUntilDone: false)
+        
+    }
+    
+    func updateMeteringUI(obj: AnyObject) {
+            let dbLevel = obj as! Float
+            print (dbLevel)
+            meteringLabel.text = "dB: " + String(dbLevel)
+    }
 
     
+    //MARK: - View initializing
+    func initPlayButtonView(canPlay : Bool)
+    {
+        
+        audioPlayLayer.removeFromSuperlayer()
+        audioPlayLayer.backgroundColor = UIColor.clearColor().CGColor
+        audioPlayLayer.path = canPlay ? audioPlayPath.CGPath : audioPausePath.CGPath
+        audioPlayLayer.fillColor = AppDelegate.cellInnerColor.CGColor
+        audioPlayLayer.fillRule = kCAFillRuleEvenOdd
+        startPlayBtn.layer.addSublayer(audioPlayLayer)
+    }
+    
+    func initRecordButtonView(canRecord : Bool) {
+        
+        audioRecordLayer.removeFromSuperlayer()
+        audioRecordLayer.backgroundColor = UIColor.clearColor().CGColor
+        audioRecordLayer.path = canRecord ? audioRecordPath.CGPath : audioStopRecordPath.CGPath
+        audioRecordLayer.fillColor = AppDelegate.cellInnerColor.CGColor
+        audioRecordLayer.fillRule = kCAFillRuleEvenOdd
+        startRecordBtn.layer.addSublayer(audioRecordLayer)
+
+    }
+    
+    func initAudioPlayPath() {
+        //// Group 2
+        //// Group 3
+        //// Bezier Drawing
+        let bezierPath = UIBezierPath()
+        bezierPath.moveToPoint(CGPoint(x: 0.08, y: 6.72))
+        bezierPath.addCurveToPoint(CGPoint(x: 9.76, y: 1.13), controlPoint1: CGPoint(x: 0.08, y: 0.54), controlPoint2: CGPoint(x: 4.48, y: -1.96))
+        bezierPath.addLineToPoint(CGPoint(x: 48.04, y: 23.35))
+        bezierPath.addCurveToPoint(CGPoint(x: 48.04, y: 34.54), controlPoint1: CGPoint(x: 53.32, y: 26.44), controlPoint2: CGPoint(x: 53.32, y: 31.45))
+        bezierPath.addLineToPoint(CGPoint(x: 9.76, y: 56.77))
+        bezierPath.addCurveToPoint(CGPoint(x: 0.08, y: 51.17), controlPoint1: CGPoint(x: 4.48, y: 59.71), controlPoint2: CGPoint(x: 0.08, y: 57.21))
+        bezierPath.addLineToPoint(CGPoint(x: 0.08, y: 6.72))
+        bezierPath.closePath()
+        bezierPath.miterLimit = 4;
+        
+        
+        audioPlayPath = bezierPath
+    }
+    
+    func initAudioPausePath() {
+        //// Group 3
+        //// Bezier Drawing
+        let bezierPath = UIBezierPath()
+        bezierPath.moveToPoint(CGPoint(x: 0.06, y: 0.5))
+        bezierPath.addLineToPoint(CGPoint(x: 18.75, y: 0.5))
+        bezierPath.addLineToPoint(CGPoint(x: 18.75, y: 62))
+        bezierPath.addLineToPoint(CGPoint(x: 0.06, y: 62))
+        bezierPath.addLineToPoint(CGPoint(x: 0.06, y: 0.5))
+        bezierPath.closePath()
+        bezierPath.moveToPoint(CGPoint(x: 30.31, y: 0.5))
+        bezierPath.addLineToPoint(CGPoint(x: 49, y: 0.5))
+        bezierPath.addLineToPoint(CGPoint(x: 49, y: 62))
+        bezierPath.addLineToPoint(CGPoint(x: 30.31, y: 62))
+        bezierPath.addLineToPoint(CGPoint(x: 30.31, y: 0.5))
+        bezierPath.closePath()
+        bezierPath.miterLimit = 4;
+    
+        audioPausePath = bezierPath
+    }
+    
+    func initAudioRecordPath() {
+        let ovalPath = UIBezierPath(ovalInRect: CGRect(x: -0.25, y: -0.25, width: 51.5, height: 51.5))
+        audioRecordPath = ovalPath
+    }
+    
+    func initAudioStopRecordPath() {
+        let bezierPath = UIBezierPath()
+        bezierPath.moveToPoint(CGPoint(x: 45.5, y: -0))
+        bezierPath.addLineToPoint(CGPoint(x: 3.5, y: -0))
+        bezierPath.addCurveToPoint(CGPoint(x: 0, y: 3.5), controlPoint1: CGPoint(x: 1.58, y: -0), controlPoint2: CGPoint(x: 0, y: 1.57))
+        bezierPath.addLineToPoint(CGPoint(x: 0, y: 45.5))
+        bezierPath.addCurveToPoint(CGPoint(x: 3.5, y: 49), controlPoint1: CGPoint(x: 0, y: 47.42), controlPoint2: CGPoint(x: 1.58, y: 49))
+        bezierPath.addLineToPoint(CGPoint(x: 45.5, y: 49))
+        bezierPath.addCurveToPoint(CGPoint(x: 49, y: 45.5), controlPoint1: CGPoint(x: 47.43, y: 49), controlPoint2: CGPoint(x: 49, y: 47.42))
+        bezierPath.addLineToPoint(CGPoint(x: 49, y: 3.5))
+        bezierPath.addCurveToPoint(CGPoint(x: 45.5, y: -0), controlPoint1: CGPoint(x: 49, y: 1.57), controlPoint2: CGPoint(x: 47.43, y: -0))
+        bezierPath.closePath()
+        bezierPath.miterLimit = 4;
+        
+        audioStopRecordPath = bezierPath
+    }
+    
+    //MARK: - Animation initializing
+    func addBtnPathAnimation(duration: NSTimeInterval, delay:NSTimeInterval, from: CGPath, to: CGPath,onLayer layer: CAShapeLayer,completion: (() -> Void)?) {
+            let animation = CABasicAnimation(keyPath: "path")
+            animation.timingFunction      = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
+            animation.fromValue           = from
+            animation.toValue             = to
+            animation.duration            = duration
+            animation.delegate            = self
+            animation.fillMode            = kCAFillModeForwards
+            animation.removedOnCompletion = false;
+            animation.beginTime           = CACurrentMediaTime() + delay
+            
+            layer.addAnimation(animation, forKey: "btn.path")
+        
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64((delay + duration) * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) { () -> Void in
+                completion?()
+            }
+    }
+
+    //MARK: - Recording controler
+
     @IBAction func startRecord(sender: UIButton) {
-        print ("start record")
-     
+        if !isRecording {
+            print ("start record")
+            
             do {
-                if let url = self.directoryURL() {
+                if let url = self.directoryURL {
                     try audioSession.setActive(true)
                     try audioRecorder = AVAudioRecorder(URL: url, settings: recordSettings)
                     audioRecorder.delegate = self
                     audioRecorder.prepareToRecord()
+                    audioRecorder.meteringEnabled = true
                 }
-                audioRecorder.record()
+                 startAudioMetering()
+                 audioRecorder.record()
+                 isRecording = true
+               //  let duration = NSTimeInterval(0.5)
+               //  let delay = 0.0
+               //  addBtnPathAnimation(duration, delay: delay, from: audioRecordPath.CGPath, to:// audioStopRecordPath.CGPath, onLayer: audioRecordLayer) {
+                    self.initRecordButtonView(false)
+               // }
             } catch {
                 print ("start recording failed: \(error)")
             }
+
+        } else {
+            guard audioRecorder != nil else {
+                print ("cannot stop recording")
+                return
+            }
+            audioRecorder.stop()
+            stopAudioMetering()
+            isRecording = false
+            self.initRecordButtonView(true)
+            do {
+                try audioSession.setActive(false)
+            } catch {
+                print ("stop recording failed: \(error)")
+            }
+
+        }
         
         
     }
     
-    @IBAction func stopRecord(sender: UIButton) {
-        print ("stop record")
-        guard audioRecorder != nil else {
-            print ("cannot stop recording")
-            return
-        }
-        audioRecorder.stop()
-        do {
-            try audioSession.setActive(false)
-        } catch {
-            print ("stop recording failed: \(error)")
-        }
-    }
     
     func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
         if flag {
@@ -91,28 +277,48 @@ class AudioViewController : UIViewController, UINavigationControllerDelegate, AV
         }
     }
     
+    
+    //MARK: - Playing control
+    
     @IBAction func startPlay(sender: UIButton) {
-        if let url = self.directoryURL() {
-            do {
-                audioPlayer = try AVAudioPlayer(contentsOfURL: url)
-                audioPlayer.play()
-            } catch  {
-                print ("playing failed: \(error)")
+        if !isPlaying {
+            if let url = self.directoryURL {
+                do {
+                    audioPlayer = try AVAudioPlayer(contentsOfURL: url)
+                    audioPlayer.prepareToPlay()
+                    audioPlayer.meteringEnabled = true
+                    audioPlayer.play()
+                } catch  {
+                    print ("playing failed: \(error)")
+                }
+                
+                print ("started play")
+                isPlaying = true
+                // let duration = NSTimeInterval(0.5)
+                // let delay = 0.0
+                // addPlayBtnPathAnimation(duration, delay: delay) {
+                self.initPlayButtonView(false)
+                // }
+                startAudioMetering()
+                
+            } else  {
+                print ("Cannot get url")
             }
-            
-            print ("started play")
-        } else  {
-            print ("Cannot get url")
+
+        } else {
+            if let ap = audioPlayer {
+                ap.stop()
+                print ("stopped play")
+                self.initPlayButtonView(true)
+                isPlaying = false
+                stopAudioMetering()
+            } else {
+                print ("audio player stopping failed")
+            }
+
         }
         
         
     }
-    
-    @IBAction func stopPlay(sender: UIButton) {
-            if let ap = audioPlayer {
-                ap.stop()
-                print ("stopped play")
-            }
-        
-    }
+   
 }
