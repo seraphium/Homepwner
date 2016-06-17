a648147b1e1a7405f968adcc3e9bd188adcb3e2a
张泽 <gjjisgod@gmail.com>
2016-06-17 11:37:24 +0800

    updated app Icon and color style

--- EasyToDoProject/EasyToDo/AppDelegate.swift
+++ EasyToDoProject/EasyToDo/AppDelegate.swift
@@ -16,18 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     static let imageStore =  ImageStore()
     static let audioStore = AudioStore()
     
-   /* static let backColor = UIColor(red: 206.0/255.0, green: 203.0/255.0, blue: 188.0/255.0, alpha: 1.0)
-    static let cellColor = UIColor(red: 169.0/255.0, green: 167.0/255.0, blue: 158.0/255.0, alpha: 0.8)
-    static let cellInnerColor = UIColor(red: 206.0/255.0, green: 203.0/255.0, blue: 188.0/255.0, alpha: 0.7)
-*/
-   /* static let backColor = UIColor(red: 147/255.0, green: 165/255.0, blue: 198/255.0, alpha: 1.0)
-    static let cellColor = UIColor(red: 238/255.0, green: 241/255.0, blue: 249.0/255.0, alpha: 1.0)
-    static let cellInnerColor = UIColor(red: 147/255.0, green: 165/255.0, blue: 198/255.0, alpha: 0.8) */
-    
-    static let backColor = UIColor(red: 225.0/255.0, green: 236.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+  /*  static let backColor = UIColor(red: 225.0/255.0, green: 236.0/255.0, blue: 255.0/255.0, alpha: 1.0)
     static let cellColor = UIColor(red: 248.0/255.0, green: 248.0/255.0, blue: 250.0/255.0, alpha: 1.0)
-    static let cellInnerColor = UIColor(red: 103.0/255.0, green: 127.0/255.0, blue: 228.0/255.0, alpha: 1.0)
+    static let cellInnerColor = UIColor(red: 103.0/255.0, green: 127.0/255.0, blue: 228.0/255.0, alpha: 1.0)*/
  
+    static let backColor = UIColor(red: 220.0/255.0, green: 237.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+    static let cellColor = UIColor(red: 253/255.0, green: 254/255.0, blue: 255.0/255.0, alpha: 1.0)
+    static let cellInnerColor = UIColor(red: 34.0/255.0, green: 149.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+    
 
     func finishItemNotification(notification : UILocalNotification) -> Item? {
         var userInfo = notification.userInfo!
