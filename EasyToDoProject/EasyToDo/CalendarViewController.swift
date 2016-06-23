cf298db96f3afabc4e3234204a34f3230843b604
张泽 <gjjisgod@gmail.com>
2016-06-23 13:38:57 +0800

    added calenderview/tableview

--- EasyToDoProject/EasyToDo/CalendarViewController.swift
+++ EasyToDoProject/EasyToDo/CalendarViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class CalendarViewController : UIViewController, CalendarViewDelegate {
     
 
-    @IBOutlet var containerView: UIView!
+    @IBOutlet weak var calenderContainerView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,13 +24,13 @@ class CalendarViewController : UIViewController, CalendarViewDelegate {
         // selected date (marked dated in the calendar)
         let calendarView = CalendarView.instance(date, selectedDate: date)
         calendarView.delegate = self
-        containerView.addSubview(calendarView)
-        
-        // Constraints for calendar view - Fill the parent view.
-        containerView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[calendarView]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["calendarView": calendarView]))
-        containerView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[calendarView]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["calendarView": calendarView]))
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
 
-        
+        calenderContainerView.addSubview(calendarView)
+        // Constraints for calendar view - Fill the parent view.
+        calenderContainerView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[calendarView]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["calendarView": calendarView]))
+        calenderContainerView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[calendarView]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["calendarView": calendarView]))
+    
     }
     
     func didSelectDate(date: NSDate) {
